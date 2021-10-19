cfccb048b66098e02b503ea1693650116374792a
Raoua Messai <raoua.messai@busit.com>
2021-10-19 15:18:34 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <md-tabs class="md-primary" md-alignment="centered">
+      <md-tab id="tab-home" md-label="Home" to="/"></md-tab>
+      <md-tab id="tab-pages" md-label="Restaurant" to="/Restaurant"></md-tab>
+    </md-tabs>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  background: fixed url(https://www.wallpaperbetter.com/wallpaper/63/966/426/vintage-restaurant-1080P-wallpaper.jpg);
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: left;
+  color: #2c3e50;
+}
+.md-tabs {
+  margin-bottom: 24px;
+}
+.md-table {
+    margin-left: 10%;
+    margin-right: 10%;
+    margin-top: 2%;
+}
+
+</style>
---
+++ src/components/Restaurant.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <md-card>
+    <h1>Détail du restaurant {{ id }}</h1>
+    <h3>Nom du Restaurant : {{ name }}</h3>
+    <h3>Cuisine : {{ cuisine }}</h3>
+    <h3>Ville : </h3>
+    <h3>Photo du restaurant: </h3>
+    <h3>Notation : </h3>
+    <h3>Emplacement Maps : </h3>
+      </md-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Restaurant',
+  props: {
+  },
+  computed: {
+      id() {
+          return this.$route.params.r
+      }
+  },
+  data: function() {
+      return {
+          restaurant: null
+      }
+  },
+  mounted() {
+      console.log("ID : " + this.id)
+      let url = "http://localhost:8081/api/restaurants/" + this.id;
+      fetch(url)
+      .then(reponse => {
+          return reponse.json();
+      }).then(data => {
+          this.restaurant = data.restaurant;
+      })   
+  },
+  methods : {
+  }
+}
+</script>
+
+<style>
+
+</style>
