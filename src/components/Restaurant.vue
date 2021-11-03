46fb98684c270566641181f5e2c201022acc6e48
ETAZI <eliastazi@yahoo.fr>
2021-11-03 23:07:01 +0100

    Correction image du restarant

--- src/components/Restaurant.vue
+++ src/components/Restaurant.vue
@@ -6,7 +6,8 @@
     <h3>Cuisine : {{ restaurant.cuisine }}</h3>
     <h3>Ville : {{ restaurant.borough }} </h3>
     <h3>Adresse : {{ restaurant.address.building }} {{ restaurant.address.street }} </h3>
-    <h3>Photo du restaurant: <img src="https://source.unsplash.com/featured/?restaurant"> </h3>
+    <h3>Photo du restaurant: </h3>
+    <RestaurantImage />
     <h3>Notation : {{ restaurant.grades }} </h3>
     <h3>Emplacement Maps : {{restaurant.address.coord[0]}} </h3>
 
@@ -20,11 +21,13 @@
 
 <script>
 import Map from './map';
+import RestaurantImage from './RestaurantImage';
 
 export default {
   name: 'Restaurant',
   components: {
-   Map
+   Map,
+   RestaurantImage
  },
   props: {
   },
@@ -48,7 +51,16 @@ export default {
           return reponse.json();
       }).then(data => {
           this.restaurant = data.restaurant;
-      })   
+      })
+      let imgUrl = "https://api.unsplash.com/photos/random/?client_id=PnvDciioIsipuy-DFRWQQmZSP3B18rVjbLRqSmqiknw&page=1&query=restaurant";
+      let imgElement = document.querySelector("#unsplashImg");
+      let imgLink = document.querySelector("#linkImg");
+      fetch(imgUrl)
+      .then((response) => response.json())
+      .then((jsonData) => {
+          imgElement.src = jsonData.urls.regular;
+          imgLink.setAttribute("href", jsonData.links.html)
+      })
   },
   
   
---
+++ src/components/RestaurantImage.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <a id="linkImg" href="#">
+            <img alt="" id="unsplashImg"/>
+        </a>
+    </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+ },
+  props: {
+  },
+  mounted() {
+      let imgUrl = "https://api.unsplash.com/photos/random/?client_id=PnvDciioIsipuy-DFRWQQmZSP3B18rVjbLRqSmqiknw&page=1&query=restaurant";
+      let imgElement = document.querySelector("#unsplashImg");
+      let imgLink = document.querySelector("#linkImg");
+      fetch(imgUrl)
+      .then((response) => response.json())
+      .then((jsonData) => {
+          imgElement.src = jsonData.urls.regular;
+          imgLink.setAttribute("href", jsonData.links.html)
+      })
+  },
+  
+  
+  methods : {
+
+  },
+  
+}
+</script>
+
+<style>
+
+</style>
